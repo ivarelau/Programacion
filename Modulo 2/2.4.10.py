310c949c04db3e908bad1ddbfc993866147cd5a4
ivarelau <ivarelau@ctpplatanares.ed.cr>
2025-06-13 16:19:17 -0600

    cotidiano Nooebook 03

--- Modulo 2/2.4.10.py
+++ Modulo 2/2.4.10.py
@@ -1,4 +1,19 @@
-x =  # Codifica tus datos de prueba aquí.
+ # Codifica tus datos de prueba aquí.
 x = float(x)
 # Escribe tu código aquí.
+print("y =", y)
+
+x = 0
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x -1
+print("y =", y)
+
+x = 1
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print("y =", y)
+
+x = -1
+float(x)
+x = 3 * x**3 - 2 * x**2 + 3 * x - 1
 print("y =", y)
--- Modulo 2/2.4.9.py
+++ Modulo 2/2.4.9.py
@@ -1,8 +1,8 @@
 kilometers = 12.25
 miles = 7.38
 
-miles_to_kilometers = ###
-kilometers_to_miles = ###
+miles_to_kilometers = miles * 7.38
+kilometers_to_miles = kilometers / 12.25
 
 print(miles, "millas son", round(miles_to_kilometers, 2), "kilómetros")
 print(kilometers, "kilómetros son", round(kilometers_to_miles, 2), "millas")
